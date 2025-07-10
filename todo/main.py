9311367e781be347a5dd50d8cd92e53df42082f4
Yorukot <hi@yorukot.me>
2025-07-10 15:25:31 +0800

    feat: refactor todo application functions for improved data handling and type annotations

--- todo/main.py
+++ todo/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from fastapi.middleware.cors import CORSMiddleware
-
+import json
 
 class TodoItem(BaseModel):
     title: str
@@ -15,47 +15,73 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # 開發環境可以用 "*"，生產環境要指定具體域名
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+def load_data():
+    todo_list : list[TodoItem] = []
+    try:
+        with open("data.json", "r") as f:
+            data = json.load(f)
+            for item in data:
+                todo_list.append(TodoItem(**item))
+        return todo_list
+    except FileNotFoundError:
+        return []
+
+def save_data(todo_list: list[TodoItem]):
+    data = []
+    with open("data.json", "w") as f:
+        for item in todo_list:
+            data.append(item.dict())
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
 
 @app.get("/")
 def root():
     return { "message" : "Hello world" }
 
 @app.get("/todos")
-def get_todos():
-    return todo_list
+def get_todos() -> list[TodoItem]:
+    return load_data()
 
 @app.post("/todos")
-def add_todo(todo: TodoItem):
+def add_todo(todo: TodoItem) -> TodoItem:
+    todo_list = load_data()
     todo_list.append(todo)
+    save_data(todo_list)
     return todo
 
 @app.put("/todo/{id}")
-def update_todo(id: int, todo: TodoItem):
+def update_todo(id: int, todo: TodoItem) -> TodoItem:
+    todo_list = load_data()
     if id < 0 or id >= len(todo_list):
         raise HTTPException(
             status_code=404,
             detail="Todo item not found"
             )
     todo_list[id] = todo
+    save_data(todo_list)
     return todo
 
 @app.delete("/todo/{id}")
 def delete_todo(id: int):
+    todo_list = load_data()
     if id < 0 or id >= len(todo_list):
         raise HTTPException(
             status_code=404,
             detail="Todo item not found"
             )
     todo_list.pop(id)
+    save_data(todo_list)
     return
 
 @app.get("/todo/{id}")
-def get_todo(id: int):
+def get_todo(id: int) -> TodoItem:
+    todo_list = load_data()
     if id < 0 or id >= len(todo_list):
         raise HTTPException(
             status_code=404,
